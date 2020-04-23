a7fa331c360f62b8ca2faf4beed2c6b811b11472
Inamkhan <inamk2111@gmail.com>
2020-04-23 15:52:02 +0500

    dev command

--- src/container/Signin.js
+++ src/container/Signin.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View, Text,StyleSheet } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator } from '@react-navigation/stack';
 import {Button,Form,Item,Input,Label} from 'native-base'
 
 
